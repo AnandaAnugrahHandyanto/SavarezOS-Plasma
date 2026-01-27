8e3ea30c67e6a11ccf206b92b11b23aa2c23f0ef
Ananda Anugrah Handyanto <168060635+AnandaAnugrahHandyanto@users.noreply.github.com>
2026-01-28 02:50:08 +0700

    fix: full branding, installer, wallpaper, identity

--- config/includes.chroot/etc/profile.d/99-savarez.sh
+++ config/includes.chroot/etc/profile.d/99-savarez.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-WP="/usr/share/savarez/savarez-wallpaper.png"
-
-# Apply wallpaper on login
-if command -v plasma-apply-wallpaperimage >/dev/null; then
-    plasma-apply-wallpaperimage "$WP" || true
-fi
-
+# Remove installer after install
 if [ -f /etc/savarez-installed ]; then
     rm -f "$HOME/Desktop/Install-SavarezOS.desktop" 2>/dev/null
 fi
--- config/includes.chroot/usr/share/plasma/layout-templates/savarezos/layout.js
+++ config/includes.chroot/usr/share/plasma/layout-templates/savarezos/layout.js
@@ -1,7 +1,7 @@
 var wallpaper = "/usr/share/savarez/savarez-wallpaper.png";
 
-var plasma = desktopsForActivity(currentActivity());
-var d = plasma[0];
+var desktops = desktopsForActivity(currentActivity());
+var d = desktops[0];
 
 d.wallpaperPlugin = "org.kde.image";
 d.currentConfigGroup = ["Wallpaper", "org.kde.image", "General"];
