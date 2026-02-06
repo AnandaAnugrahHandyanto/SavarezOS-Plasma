82ca52c521b62ed63f3405c389e91cb5aa33e528
Ananda Anugrah Handyanto <168060635+AnandaAnugrahHandyanto@users.noreply.github.com>
2026-02-06 19:55:03 +0000

    fix: stabilize live environment, installer, sddm theme, and UEFI bootloader

--- config/includes.chroot/etc/calamares/scripts/install-grub.sh
+++ config/includes.chroot/etc/calamares/scripts/install-grub.sh
@@ -2,26 +2,21 @@
 set -e
 
 TARGET=/target
-ESP=/boot/efi
 
 mount --bind /dev  $TARGET/dev
 mount --bind /proc $TARGET/proc
 mount --bind /sys  $TARGET/sys
 
-# Mount ESP
-chroot $TARGET mkdir -p $ESP
-chroot $TARGET mount $(lsblk -rpno NAME,PARTTYPE | grep c12a7328 | cut -d' ' -f1 | head -n1) $ESP || true
+# Mount EFI
+if ! mountpoint -q $TARGET/boot/efi; then
+  EFI=$(lsblk -rpno NAME,PARTTYPE | grep c12a7328 | cut -d' ' -f1 | head -n1)
+  mount "$EFI" $TARGET/boot/efi
+fi
 
-# Install grub
 chroot $TARGET grub-install \
   --target=x86_64-efi \
-  --efi-directory=$ESP \
+  --efi-directory=/boot/efi \
   --bootloader-id=SavarezOS \
   --recheck
-
-# Fallback
-chroot $TARGET mkdir -p $ESP/EFI/BOOT
-chroot $TARGET cp $ESP/EFI/SavarezOS/grubx64.efi \
-  $ESP/EFI/BOOT/BOOTX64.EFI || true
 
 chroot $TARGET update-grub
--- config/includes.chroot/usr/share/sddm/themes/savarez/SessionButton.qml
+++ config/includes.chroot/usr/share/sddm/themes/savarez/SessionButton.qml
@@ -5,10 +5,11 @@
     SPDX-License-Identifier: LGPL-2.0-or-later
 */
 
-import QtQuick 2.15
+import QtQuick 2.11
 
 import org.kde.plasma.components 3.0 as PlasmaComponents
-import org.kde.kirigami 2.20 as Kirigami
+import org.kde.kirigami 2.15 as Kirigami
+
 
 PlasmaComponents.ToolButton {
     id: root
